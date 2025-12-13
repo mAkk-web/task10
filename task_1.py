04feb94ea8eda59515bfa2978ba8571c089e18f1
mAkk-web <makaridze.ua@gmail.com>
2025-12-13 12:02:11 +0200

    Added cycle and fixed

--- task_1.py
+++ task_1.py
@@ -1,1 +1,12 @@
-print("Hello World")
+B = list(map(int, input("Введіть елементи списку через пробіл: ").split()))
+
+s = 0
+
+if 0 in B:
+    Zero_index = B.index(0)
+    for i in range(Zero_index+1, len(B)):
+       s+=abs(B[i])
+else:
+    s = 0
+
+print("Сума модулів після першого нуля:", s)
